d3ef5c0977f18cf335f07e0db3588cc72009ae5b
Niterich <52295078+Niterich@users.noreply.github.com>
2020-01-07 10:14:59 -0700

    ajax functions properly, need to update readme and finish local storage and geolocation functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,43 @@
 const now = moment().format("MM/DD/YYYY")
 let cityName;
-let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",us&mode=json&units=imperial&appid=88ac7af34cb90d87533bbf879a2bb928";
+let queryURL;
+let historyArray = [JSON.parse(JSON.stringify(localStorage.getItem("history")))] || [];
 
 $("#searchButton").on("click", function(){
-  cityName = $(this).prev().val();
-  console.log(cityName);
-  // localStorage.setItem($(this).val(), savedCity);
-  // let newSavedCity = $("<button>").text(cityName);
-  // console.log(newSavedCity);
-  // $("cityList").append(newSavedCity);
-  // this.blur();
+  event.preventDefault();
+  clear();
+  requestWeather($(this).prev().val());
+  createButton($(this).prev().val());
+});
+
+$("#cityList").on("click", ".savedCityButton", function(){
+  event.preventDefault();
+  clear();
+  requestWeather($(this).text());
+});
+
+// $( document ).ready(function() {
+//   console.log( "ready!" );
+//   for (let i = 1; i < historyArray.length; i++) {
+//     createButton(historyArray[i]);
+//     console.log(historyArray[i]);
+//   }
+// });
+
+
+function createButton(cityName){
+  const newSavedCity  = $("<button>").text(cityName).addClass("savedCityButton btn btn-secondary");
+  $("#cityList").append(newSavedCity);
+};
+
+function requestWeather(cityName) {
+  // cityName = $(this).prev().val();
+  queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",us&mode=json&units=imperial&appid=88ac7af34cb90d87533bbf879a2bb928";
+  if (historyArray.indexOf(cityName) === -1){
+    historyArray.push(cityName);
+  };
+  localStorage.setItem("history", historyArray);
+  this.blur();
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -26,10 +54,35 @@ $("#searchButton").on("click", function(){
     const lon = res.coord.lon;
     $.ajax({
       url: "http://api.openweathermap.org/data/2.5/uvi?appid=88ac7af34cb90d87533bbf879a2bb928&lat="+ lat + "&lon=" + lon,
-      method: "GET"
+      method: "GET",
     }).then(function(resTwo){
       console.log(resTwo);
       $("#mainUvIndexSpan").text(resTwo.value);
     })
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",us&mode=json&units=imperial&appid=88ac7af34cb90d87533bbf879a2bb928",
+      method: "GET"
+    }).then(function(resThree){
+      console.log(resThree)
+      for (let i = 0; i < resThree.list.length; i++) {
+        let forecastDay = resThree.list[i];
+        if (forecastDay.dt_txt.indexOf("18:00") !== -1){
+          console.log(true);
+          const dayCard = $("<div>").addClass("card col-2");
+          const cardDate = $("<p>").text(forecastDay.dt_txt.substr(5, 5)).appendTo(dayCard);
+          const forecastImage = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + forecastDay.weather[0].icon + "@2x.png").appendTo(dayCard);
+          const forecastTemp = $("<p>").text("Temperature: " + forecastDay.main.temp.toFixed(1) +" °F").appendTo(dayCard);
+          const forecastHum = $("<p>").text("Humidity: " + forecastDay.main.humidity + "%").appendTo(dayCard);
+          $("#fiveDayForecastDiv").append(dayCard);
+        }
+      }
+    })
   });
-});
+}
+
+function clear(){
+  $("#fiveDayForecastDiv").empty();
+}
+
+//Create the buttons for anything in local storage on page load
+//use Geolocation for initial page load
