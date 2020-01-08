61986d00eb33d1cdaf073be9a8de1d9523f62542
Niterich <52295078+Niterich@users.noreply.github.com>
2020-01-07 18:33:34 -0700

    MVP works

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -32,7 +32,7 @@ function createButton(cityName){
 
 function requestWeather(cityName) {
   // cityName = $(this).prev().val();
-  queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",us&mode=json&units=imperial&appid=88ac7af34cb90d87533bbf879a2bb928";
+  queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",us&mode=json&units=imperial&appid=88ac7af34cb90d87533bbf879a2bb928";
   if (historyArray.indexOf(cityName) === -1){
     historyArray.push(cityName);
   };
@@ -42,35 +42,32 @@ function requestWeather(cityName) {
     url: queryURL,
     method: "GET"
   }).then(function(res) {
-    console.log(res);
     $("#cityName").text(res.name + "  ");
     $("#mainTempSpan").text((res.main.temp).toFixed(1));
     $("#mainHumiditySpan").text(res.main.humidity);
     $("#mainWindSpan").text(res.wind.speed);
-    const weatherImage = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + res.weather[0].icon + "@2x.png");
+    const weatherImage = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + res.weather[0].icon + "@2x.png");
     $("#date").text(" " + now);
     $("#date").append(weatherImage);
     const lat = res.coord.lat;
     const lon = res.coord.lon;
     $.ajax({
-      url: "http://api.openweathermap.org/data/2.5/uvi?appid=88ac7af34cb90d87533bbf879a2bb928&lat="+ lat + "&lon=" + lon,
+      url: "https://api.openweathermap.org/data/2.5/uvi?appid=88ac7af34cb90d87533bbf879a2bb928&lat="+ lat + "&lon=" + lon,
       method: "GET",
     }).then(function(resTwo){
-      console.log(resTwo);
       $("#mainUvIndexSpan").text(resTwo.value);
     })
     $.ajax({
-      url: "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",us&mode=json&units=imperial&appid=88ac7af34cb90d87533bbf879a2bb928",
+      url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",us&mode=json&units=imperial&appid=88ac7af34cb90d87533bbf879a2bb928",
       method: "GET"
     }).then(function(resThree){
-      console.log(resThree)
       for (let i = 0; i < resThree.list.length; i++) {
         let forecastDay = resThree.list[i];
         if (forecastDay.dt_txt.indexOf("18:00") !== -1){
           console.log(true);
           const dayCard = $("<div>").addClass("card col-2");
           const cardDate = $("<p>").text(forecastDay.dt_txt.substr(5, 5)).appendTo(dayCard);
-          const forecastImage = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + forecastDay.weather[0].icon + "@2x.png").appendTo(dayCard);
+          const forecastImage = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + forecastDay.weather[0].icon + "@2x.png").appendTo(dayCard);
           const forecastTemp = $("<p>").text("Temperature: " + forecastDay.main.temp.toFixed(1) +" °F").appendTo(dayCard);
           const forecastHum = $("<p>").text("Humidity: " + forecastDay.main.humidity + "%").appendTo(dayCard);
           $("#fiveDayForecastDiv").append(dayCard);
